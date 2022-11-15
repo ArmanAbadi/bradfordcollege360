cceee637c6685f74979f1702ffd0b97b5ebedd73
ArmanAbadi <arman_abadi@hotmail.com>
2022-11-14 19:37:57 -0800

    Update app.py

--- YoutubeWebAPI/youtube_dl_server/app.py
+++ YoutubeWebAPI/youtube_dl_server/app.py
@@ -9,9 +9,6 @@ from flask_limiter.util import get_remote_address
 import yt_dlp
 from yt_dlp.version import __version__ as youtube_dl_version
 
-import youtube_dl.utils
-youtube_dl.utils.std_headers['User-Agent'] = ''
-
 from .version import __version__
 
 if not hasattr(sys.stderr, 'isatty'):
